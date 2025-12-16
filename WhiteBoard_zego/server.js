d3cf0846f888d350c91e5e20b828bac3c2e7587e
Md Mihsan <mdmihsan2@gmail.com>
2025-12-16 20:58:27 +0600

    update server.js

--- WhiteBoard_zego/server.js
+++ WhiteBoard_zego/server.js
@@ -20,7 +20,7 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
